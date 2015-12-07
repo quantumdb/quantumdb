f83635d9a9c07f5de197b04da2f4a3adc44f0b70
Michael de Jong <michaelj@minicom.nl>
2015-12-07 23:56:27 +0100

    Refactored the remainder of the PostgresqlMigrator class to use the RefLog class.

--- quantumdb-core/src/main/java/io/quantumdb/core/backends/postgresql/PostgresqlMigrator.java
+++ quantumdb-core/src/main/java/io/quantumdb/core/backends/postgresql/PostgresqlMigrator.java
@@ -3,17 +3,14 @@ package io.quantumdb.core.backends.postgresql;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.HashMultimap;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
 import com.google.common.collect.Table.Cell;
@@ -33,7 +30,6 @@ import io.quantumdb.core.state.RefLog.TableRef;
 import io.quantumdb.core.utils.QueryBuilder;
 import io.quantumdb.core.versioning.MigrationFunctions;
 import io.quantumdb.core.versioning.State;
-import io.quantumdb.core.versioning.TableMapping;
 import io.quantumdb.core.versioning.Version;
 import lombok.extern.slf4j.Slf4j;
 
@@ -245,8 +241,12 @@ class PostgresqlMigrator implements DatabaseMigrator {
 		private void synchronizeForwards(Table targetTable, Set<String> targetColumns) throws SQLException {
 			log.info("Creating forward sync function for table: {}...", targetTable.getName());
 			try (Connection connection = backend.connect()) {
-				for (DataMapping dataMapping : listDataMappings(Direction.FORWARDS)) {
-					if (dataMapping.getTargetTable().equals(targetTable)) {
+				Catalog catalog = state.getCatalog();
+				Multimap<TableRef, TableRef> tableMapping = state.getRefLog().getTableMapping(from, to);
+				for (Entry<TableRef, TableRef> entry : tableMapping.entries()) {
+					if (entry.getValue().getTableId().equals(targetTable.getName())) {
+						TableRef source = entry.getKey();
+						TableRef target = entry.getValue();
 						ensureSyncFunctionExists(connection, refLog, source, target, catalog, targetColumns);
 					}
 				}
@@ -256,10 +256,14 @@ class PostgresqlMigrator implements DatabaseMigrator {
 		private void copyData(Table targetTable, Set<String> migratedColumns, Set<String> columnsToMigrate)
 				throws SQLException, InterruptedException {
 
-			for (DataMapping dataMapping : listDataMappings(Direction.FORWARDS)) {
-				if (dataMapping.getTargetTable().equals(targetTable)) {
-					TableDataMigrator tableDataMigrator = new TableDataMigrator(backend, dataMapping);
-					tableDataMigrator.migrateData(nullRecords, migratedColumns, columnsToMigrate);
+			Catalog catalog = state.getCatalog();
+			Multimap<TableRef, TableRef> tableMapping = state.getRefLog().getTableMapping(from, to);
+			for (Entry<TableRef, TableRef> entry : tableMapping.entries()) {
+				if (entry.getValue().getTableId().equals(targetTable.getName())) {
+					Table source = catalog.getTable(entry.getKey().getTableId());
+					Table target = catalog.getTable(entry.getValue().getTableId());
+					TableDataMigrator tableDataMigrator = new TableDataMigrator(backend, refLog);
+					tableDataMigrator.migrateData(nullRecords, source, target, migratedColumns, columnsToMigrate);
 				}
 			}
 		}
@@ -267,13 +271,19 @@ class PostgresqlMigrator implements DatabaseMigrator {
 		private void synchronizeBackwards() throws MigrationException {
 			log.info("Creating backwards sync functions...");
 			try (Connection connection = backend.connect()) {
-				for (DataMapping dataMapping : listDataMappings(DataMappings.Direction.BACKWARDS)) {
-					Set<String> columns = dataMapping.getTargetTable().getColumns().stream()
+				Catalog catalog = state.getCatalog();
+				Multimap<TableRef, TableRef> tableMapping = state.getRefLog().getTableMapping(from, to);
+				for (Entry<TableRef, TableRef> entry : tableMapping.entries()) {
+					TableRef source = entry.getKey();
+					TableRef target = entry.getValue();
+					Table targetTable = catalog.getTable(target.getTableId());
+
+					Set<String> columns = targetTable.getColumns().stream()
 							.map(Column::getName)
 							.collect(Collectors.toSet());
 
-					log.info("Creating backward sync function for table: {}...", dataMapping.getTargetTable().getName());
-					ensureSyncFunctionExists(connection, dataMapping, columns);
+					log.info("Creating backward sync function for table: {}...", target.getName());
+					ensureSyncFunctionExists(connection, refLog, source, target, catalog, columns);
 				}
 			}
 			catch (SQLException e) {
@@ -281,68 +291,11 @@ class PostgresqlMigrator implements DatabaseMigrator {
 			}
 		}
 
-		private List<DataMapping> listDataMappings(Direction direction) {
-			TableMapping tableMapping = state.getTableMapping();
-			Version target = direction == Direction.FORWARDS ? from : to;
-			ImmutableSet<String> tableIds = tableMapping.getTableIds(target);
-			Multimap<String, String> ghostTableIdMapping = tableMapping.getGhostTableIdMapping(from, to);
-
-			Catalog catalog = state.getCatalog();
-
-			List<DataMapping> results = Lists.newArrayList();
-			for (String tableId : tableIds) {
-				Table table = catalog.getTable(tableId);
-
-				if (direction == Direction.BACKWARDS) {
-					dataMappings.getTransitiveDataMappings(table, direction).stream()
-							.filter(mapping -> ghostTableIdMapping.containsKey(mapping.getTargetTable().getName()))
-							.forEach(results::add);
-				}
-				else {
-					dataMappings.getTransitiveDataMappings(table, direction).stream()
-							.filter(mapping -> ghostTableIdMapping.containsValue(mapping.getTargetTable().getName()))
-							.forEach(results::add);
-				}
-			}
-
-			Collections.sort(results, new Comparator<DataMapping>() {
-				@Override
-				public int compare(DataMapping o1, DataMapping o2) {
-					String t1 = o1.getTargetTable().getName();
-					String t2 = o2.getTargetTable().getName();
-
-					List<Step> migrationSteps = plan.getSteps();
-					for (Step step : migrationSteps) {
-						Set<String> tableNames = step.getOperation().getTables().stream()
-								.map(Table::getName)
-								.collect(Collectors.toSet());
-
-						if (tableNames.contains(t1)) {
-							if (tableNames.contains(t2)) {
-								return 0;
-							}
-							else {
-								return -1;
-							}
-						}
-						else {
-							if (tableNames.contains(t2)) {
-								return 1;
-							}
-						}
-					}
-					return 0;
-				}
-			});
-
-			return results;
-		}
-
 		void ensureSyncFunctionExists(Connection connection, RefLog refLog, TableRef source,
 				TableRef target, Catalog catalog, Set<String> columns) throws SQLException {
 
 			String sourceTableId = source.getTableId();
-			String targetTableId = target.getTableId()
+			String targetTableId = target.getTableId();
 
 			MigrationFunctions functions = state.getFunctions();
 			SyncFunction syncFunction = syncFunctions.get(sourceTableId, targetTableId);
