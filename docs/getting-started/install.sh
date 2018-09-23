ac9eac394dcd249406526a78ba82208d9f90c977
Michael de Jong <michaelj@minicom.nl>
2018-09-23 16:37:08 +0200

    Releasing quantumdb-0.4.0.

--- docs/getting-started/install.sh
+++ docs/getting-started/install.sh
@@ -3,8 +3,8 @@
 LIB=~/quantumdb
 mkdir -p $LIB
 cd $LIB
-curl -# -O -L 'https://github.com/quantumdb/quantumdb/releases/download/quantumdb-0.3.0/quantumdb-cli-0.3.0.jar'
+curl -# -O -L 'https://github.com/quantumdb/quantumdb/releases/download/quantumdb-0.4.0/quantumdb-cli-0.4.0.jar'
 
 echo '#!/bin/sh
-java -jar '$LIB'/quantumdb-cli-0.3.0.jar "$@"' > /usr/local/bin/quantumdb
+java -jar '$LIB'/quantumdb-cli-0.4.0.jar "$@"' > /usr/local/bin/quantumdb
 chmod +x /usr/local/bin/quantumdb
